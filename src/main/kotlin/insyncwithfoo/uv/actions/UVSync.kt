51039291ceba8763238a9afd0ffd2abc07943e23
InSyncWithFoo <insyncwithfoo@gmail.com>
2024-07-22 09:22:08 +0000

    Allow running `uv sync` as intention from `pyproject.toml`

--- src/main/kotlin/insyncwithfoo/uv/actions/UVSync.kt
+++ src/main/kotlin/insyncwithfoo/uv/actions/UVSync.kt
@@ -2,40 +2,16 @@ package insyncwithfoo.uv.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
-import com.intellij.openapi.project.Project
-import com.jetbrains.python.packaging.management.PythonPackageManager
+import insyncwithfoo.uv.intentions.syncThenReload
 import insyncwithfoo.uv.message
-import insyncwithfoo.uv.runInBackground
-import insyncwithfoo.uv.sdk
 import insyncwithfoo.uv.somethingIsWrong
-import insyncwithfoo.uv.uv
 
 
-internal class PipSync : DumbAwareAction() {
+internal class UVSync : DumbAwareAction() {
     
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project ?: return noProjectFound()
-        
-        project.runSyncCommand()
-        project.reloadPackages()
-    }
-    
-    private fun Project.runSyncCommand() {
-        val uv = uv ?: return noExecutableFound()
-        
-        runInBackground("Synchronizing...") {
-            uv.sync()
-        }
-    }
-    
-    @Suppress("UnstableApiUsage")
-    private fun Project.reloadPackages() {
-        val sdk = this.sdk ?: return
-        val uvPackageManager = PythonPackageManager.forSdk(this, sdk)
-        
-        runInBackground("Reload packages") {
-            uvPackageManager.reloadPackages()
-        }
+        project.syncThenReload()
     }
     
     private fun noProjectFound() {
@@ -43,10 +19,6 @@ internal class PipSync : DumbAwareAction() {
             message("messages.noProjectFound.title"),
             message("messages.noProjectFound.body")
         )
-    }
-    
-    private fun noExecutableFound() {
-        somethingIsWrong(message("messages.noExecutableFound.body"))
     }
     
 }
---
+++ src/main/kotlin/insyncwithfoo/uv/intentions/UVSyncFromPyProjectToml.kt
@@ -0,0 +1,64 @@
+package insyncwithfoo.uv.intentions
+
+import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.jetbrains.python.codeInsight.intentions.PyBaseIntentionAction
+import com.jetbrains.python.packaging.management.PythonPackageManager
+import insyncwithfoo.uv.message
+import insyncwithfoo.uv.runInBackground
+import insyncwithfoo.uv.sdk
+import insyncwithfoo.uv.somethingIsWrong
+import insyncwithfoo.uv.uv
+
+
+private fun noExecutableFound() {
+    somethingIsWrong(message("messages.noExecutableFound.body"))
+}
+
+
+private fun Project.runSyncCommand() {
+    val uv = this.uv ?: return noExecutableFound()
+    
+    @Suppress("UnstableApiUsage")
+    runInBackground("Synchronizing...") {
+        writeAction { uv.sync() }
+    }
+}
+
+
+@Suppress("UnstableApiUsage")
+private fun Project.reloadPackages() {
+    val sdk = this.sdk ?: return
+    val uvPackageManager = PythonPackageManager.forSdk(this, sdk)
+    
+    runInBackground("Reload packages") {
+        uvPackageManager.reloadPackages()
+    }
+}
+
+
+internal fun Project.syncThenReload() {
+    runSyncCommand()
+    reloadPackages()
+}
+
+
+internal class UVSyncFromPyProjectToml : PyBaseIntentionAction() {
+    
+    override fun startInWriteAction() = true
+    
+    override fun getFamilyName() = message("intentions.sync.familyName")
+    
+    override fun getText() = familyName
+    
+    override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
+        return file?.virtualFile?.name == "pyproject.toml"
+    }
+    
+    override fun doInvoke(project: Project, editor: Editor?, file: PsiFile?) {
+        project.syncThenReload()
+    }
+    
+}
